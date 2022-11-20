753b6fd1e77b099e3f2160b3e4563d839ee7b555
mibrgmv <mibrgmv@gmail.com>
2022-11-21 00:52:51 +0300

    Добавил ограничения для функций тангенса и котангенса

--- trigonometry.py
+++ trigonometry.py
@@ -27,16 +27,18 @@ def cos(x):
 def tg(x):
     if not cos(x) == 0:
         return round(sin(x) / cos(x), 4)
+    else:
+        return None
 
 
-def cotangent(x):
+def ctg(x):
     if not sin(x) == 0:
         return round(cos(x) / sin(x), 4)
+    else:
+        return None
 
 
 #  + 429*x**15/30720
 def arcsin(x):
     x = x + x**3/6 + 3*x**5/40 + 5*x**7/112 + 35*x**9/1152 + 81*x**11/2816 + 231*x**13/13312
     return x
-
-
