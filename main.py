a06459d5526ae81bd8004c6cbef6317d0430c806
say1dead <107905872+say1dead@users.noreply.github.com>
2022-11-23 21:25:24 +0300

    Финальная версия калькулятора

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import numpy as np
+def determinant(A, total=0):
+    indices = list(range(len(A)))
+    if len(A) == 2 and len(A[0]) == 2:
+        val = A[0][0] * A[1][1] - A[1][0] * A[0][1]
+        return val
+
+    for fc in indices:
+        As = A
+        As = As[1:]
+        height = len(As)
+        for i in range(height):
+            As[i] = As[i][0:fc] + As[i][fc + 1:]
+        sign = (-1) ** (fc % 2)
+        sub_det = determinant(As)
+        total += sign * A[0][fc] * sub_det
+    return total
 
 def ln(x):
     if x > 0:
-        value = 10**8 * ((x ** (1/10**8)) - 1)
+        value = 10 ** 8 * ((x ** (1 / 10 ** 8)) - 1)
         return value
     else:
         return None
-        
+
+
 def log(x, base):
     if x > 0 and base > 0:
         value = ln(x) / ln(base)
         return value
     else:
         return None
+
 
 def factorial(x):
     for i in range(1, x):
@@ -21,21 +38,21 @@ def factorial(x):
 
 
 def sin(x):
-    x *= 3.1415926/180
+    x *= 3.1415926 / 180
     value = x
     sign = -1
     for i in range(3, 100, 2):
-        value += (x**i / factorial(i) * sign)
+        value += (x ** i / factorial(i) * sign)
         sign *= -1
     return round(value, 4)
 
 
 def cos(x):
-    x *= 3.1415926/180
+    x *= 3.1415926 / 180
     value = 1
     sign = -1
     for i in range(2, 100, 2):
-        value += (x**i / factorial(i) * sign)
+        value += (x ** i / factorial(i) * sign)
         sign *= -1
     return round(value, 4)
 
@@ -56,18 +73,23 @@ def ctg(x):
 
 #  + 429*x**15/30720
 def arcsin(x):
-    if -1 <= x <= 1:
+    if -1 < x < 1:
         x = x + x**3/6 + 3*x**5/40 + 5*x**7/112 + 35*x**9/1152 + 81*x**11/2816 + 231*x**13/13312 + 429*x**15/30720
         return x
+    elif x == 1 or x == -1:
+        return x * 1.57079632679
     else:
         return None
 
 def arccos(x):
-    if -1 <= x <= 1:
+    if -1 <= x < 1:
         x = 3.141592653589793238/2 - arcsin(x)
         return x
+    elif x == 1:
+        return 0
     else:
         return None
+
 
 def arctg(x):
     x = arcsin(x / ((1 + x ** 2) ** 0.5))
@@ -78,12 +100,15 @@ def arcctg(x):
     x = arccos(x / ((1 + x ** 2) ** 0.5))
     return x
 
-k = int(input("В каком модуле хотите выполнять вычисления:\n1. Простые вычисление.\n2. Тригонометрия.\n3. Матрицы.\nВаш выбор - "))
+
+k = int(input(
+    "В каком модуле хотите выполнять вычисления:\n1. Простые вычисление.\n2. Тригонометрия.\n3. Матрицы.\nВаш выбор - "))
 print("---------------\n")
 if k == 1:
     print("---------------\n")
-    c = int(input("Что хотите сделать:\n1. Сложение.\n2. Вычитание.\n3. Умножение.\n4. Деление.\n5. Возведение в степень."
-                  "\n6. Извлечение из под корня.\n7. Вычислить натуральный логарифм.\n8. Логарифм.\nВаш выбор -  "))
+    c = int(
+        input("Что хотите сделать:\n1. Сложение.\n2. Вычитание.\n3. Умножение.\n4. Деление.\n5. Возведение в степень."
+              "\n6. Извлечение из под корня.\n7. Вычислить натуральный логарифм.\n8. Логарифм.\nВаш выбор -  "))
     if c == 1:
         a = int(input("Введите первое число - "))
         b = int(input("Введите второе число - "))
@@ -113,7 +138,7 @@ if k == 1:
         a = int(input("Введите первое число - "))
         b = int(input("Введите степень корня - "))
         print("---------------\n")
-        print("Результат - ", (a)**(1/b))
+        print("Результат - ", (a) ** (1 / b))
     if c == 7:
         a = int(input("Введите число - "))
         print("---------------\n")
@@ -126,7 +151,7 @@ if k == 1:
 
 if k == 2:
     k = int(input("Что хотим посчитать:\n1. Синус (sin).\n2. Косинус (cos).\n3. Тангенс (tg).\n4. Котангенс (ctg)."
-                  "\n5. Арксинус (arcsin).\n6. Арккосинус (arccos).\n7. Арктангесн (arctg).\n8. Арккотангенс (arcctg).\nВаш выбор - "))
+                  "\n5. Арксинус (arcsin).\n6. Арккосинус (arccos).\n7. Арктангенс (arctg).\n8. Арккотангенс (arcctg).\nВаш выбор - "))
     print("---------------\n")
     if k == 1:
         x = float(input("Введите угол в градусах - "))
@@ -169,7 +194,7 @@ if k == 3:
     mat1 = []
     mat2 = []
     mat3 = []
-
+    #сложение
     if k == 1:
         m = int(input("Введите количество столбцов -"))
         n = int(input("Введите количество строк -"))
@@ -201,16 +226,16 @@ if k == 3:
                     print("---------------\n")
                     print(mat3[i][j], end=" ")
                 print()
-
+    #вычитание
     if k == 2:
         m = int(input("Введите количество столбцов -"))
         n = int(input("Введите количество строк -"))
         m1 = int(input("Введите количество столбцов -"))
         n1 = int(input("Введите количество строк -"))
-        
+
         if m != m1 and n != n1:
             print("Вычитание матриц возможно только для матриц одинаковых размеров")
-        
+
         if m == n:
 
             print("Введите первую матрицу -")
@@ -238,7 +263,7 @@ if k == 3:
                     print("---------------\n")
                     print("Результат - ", mat3[i][j], end=' ')
                 print()
-
+    #произведение
     if k == 3:
         m = int(input("Введите количество столбцов 1 матрицы -"))
         n = int(input("Введите количество строк 1 матрицы -"))
@@ -246,7 +271,8 @@ if k == 3:
         n1 = int(input("Введите количество строк 2 матрицы -"))
 
         if n != m1:
-            print("Для проивзедения матриц нужно, чтобы количество строк 1 матрицы было равно количеству столбцов 2 матрицы")
+            print(
+                "Для проивзедения матриц нужно, чтобы количество строк 1 матрицы было равно количеству столбцов 2 матрицы")
 
         if n == m1:
 
@@ -263,10 +289,19 @@ if k == 3:
                 for j in range(n1):
                     b.append(int(input()))
                 mat2.append(b)
-
+            if n != m:
+                print("Произведение матриц возможно при одинаковом количесте столбцов первой матрицы и строк второй матрицы")
+            mat3 = [[0 for m in range(m)] for n1 in range(n1)]
+            for i in range(m):
+                for j in range(n1):
+                    for k in range(n):
+                        mat3[i][j] += mat1[i][k] * mat2[k][j]
             print("---------------\n")
-            print("Произведение матриц = \n", np.dot(mat1, mat2))
-
+            for i in range(m):
+                for j in range(n1):
+                    print(mat3[i][j], end=' ')
+                print()
+    #умножение на константу
     if k == 4:
         m = int(input("Введите количество столбцов -"))
         n = int(input("Введите количество строк -"))
@@ -290,6 +325,7 @@ if k == 3:
                 for j in range(n):
                     print(mat3[i][j], end=" ")
                 print()
+    #транспонирование
     if k == 5:
         m = int(input("Введите количество столбцов -"))
         n = int(input("Введите количество строк -"))
@@ -303,10 +339,14 @@ if k == 3:
                 a.append(int(input()))
             mat1.append(a)
         if m == n:
-            mat2 = np.transpose(mat1)
+            mat2 = list(zip(*mat1))
         print("---------------\n")
-        print("Транспонированная - \n", mat2)
-
+        print("Транспонированная - \n")
+        for i in range(m):
+            for j in range(n):
+                print(mat2[i][j], end=' ')
+            print()
+    #определитель
     if k == 6:
         m = int(input("Введите количество столбцов -"))
         n = int(input("Введите количество строк -"))
@@ -333,9 +373,11 @@ if k == 3:
                 for j in range(n):
                     a.append(int(input()))
                 mat1.append(a)
-            r1 = (mat1[0][0] * mat1[1][1] * mat1[2][2] + mat1[0][1] * mat1[1][2] * mat1[2][0] + mat1[1][0] * mat1[2][1] *
+            r1 = (mat1[0][0] * mat1[1][1] * mat1[2][2] + mat1[0][1] * mat1[1][2] * mat1[2][0] + mat1[1][0] * mat1[2][
+                1] *
                   mat1[0][2])
-            r2 = (mat1[0][2] * mat1[1][1] * mat1[2][0] + mat1[0][1] * mat1[1][0] * mat1[2][2] + mat1[0][0] * mat1[1][2] *
+            r2 = (mat1[0][2] * mat1[1][1] * mat1[2][0] + mat1[0][1] * mat1[1][0] * mat1[2][2] + mat1[0][0] * mat1[1][
+                2] *
                   mat1[2][1])
             print("---------------\n")
             print("Определитель = ", r1 - r2)
@@ -347,18 +389,26 @@ if k == 3:
                 for j in range(n):
                     a.append(int(input()))
                 mat1.append(a)
-            r1 = (mat1[1][1] * mat1[2][2] * mat1[3][3] + mat1[1][2] * mat1[2][3] * mat1[3][1] + mat1[2][1] * mat1[3][2] *
-                  mat1[1][3]) - (mat1[1][3] * mat1[2][2] * mat1[3][1] + mat1[1][2] * mat1[2][1] * mat1[3][3] + mat1[1][1] *
-                                 mat1[2][3] * mat1[3][2])
-            r2 = (mat1[1][0] * mat1[2][2] * mat1[3][3] + mat1[1][2] * mat1[2][3] * mat1[3][0] + mat1[1][3] * mat1[2][0] *
-                  mat1[3][2]) - (mat1[1][3] * mat1[2][2] * mat1[3][0] + mat1[3][2] * mat1[2][3] * mat1[1][0] + mat1[2][0] *
-                                 mat1[1][2] * mat1[3][3])
-            r3 = (mat1[1][0] * mat1[2][1] * mat1[3][3] + mat1[2][0] * mat1[3][1] * mat1[1][3] + mat1[1][1] * mat1[2][3] *
-                  mat1[3][0]) - (mat1[1][3] * mat1[2][1] * mat1[3][0] + mat1[2][0] * mat1[1][1] * mat1[3][3] + mat1[3][1] *
-                                 mat1[2][3] * mat1[1][0])
-            r4 = (mat1[1][0] * mat1[2][1] * mat1[3][2] + mat1[2][0] * mat1[3][1] * mat1[1][2] + mat1[1][1] * mat1[2][2] *
-                  mat1[3][0]) - (mat1[1][2] * mat1[2][1] * mat1[3][0] + mat1[3][1] * mat1[2][2] * mat1[1][0] + mat1[2][0] *
-                                 mat1[1][1] * mat1[3][2])
+            r1 = (mat1[1][1] * mat1[2][2] * mat1[3][3] + mat1[1][2] * mat1[2][3] * mat1[3][1] + mat1[2][1] * mat1[3][
+                2] *
+                  mat1[1][3]) - (
+                             mat1[1][3] * mat1[2][2] * mat1[3][1] + mat1[1][2] * mat1[2][1] * mat1[3][3] + mat1[1][1] *
+                             mat1[2][3] * mat1[3][2])
+            r2 = (mat1[1][0] * mat1[2][2] * mat1[3][3] + mat1[1][2] * mat1[2][3] * mat1[3][0] + mat1[1][3] * mat1[2][
+                0] *
+                  mat1[3][2]) - (
+                             mat1[1][3] * mat1[2][2] * mat1[3][0] + mat1[3][2] * mat1[2][3] * mat1[1][0] + mat1[2][0] *
+                             mat1[1][2] * mat1[3][3])
+            r3 = (mat1[1][0] * mat1[2][1] * mat1[3][3] + mat1[2][0] * mat1[3][1] * mat1[1][3] + mat1[1][1] * mat1[2][
+                3] *
+                  mat1[3][0]) - (
+                             mat1[1][3] * mat1[2][1] * mat1[3][0] + mat1[2][0] * mat1[1][1] * mat1[3][3] + mat1[3][1] *
+                             mat1[2][3] * mat1[1][0])
+            r4 = (mat1[1][0] * mat1[2][1] * mat1[3][2] + mat1[2][0] * mat1[3][1] * mat1[1][2] + mat1[1][1] * mat1[2][
+                2] *
+                  mat1[3][0]) - (
+                             mat1[1][2] * mat1[2][1] * mat1[3][0] + mat1[3][1] * mat1[2][2] * mat1[1][0] + mat1[2][0] *
+                             mat1[1][1] * mat1[3][2])
             r = mat1[0][0] * r1 - mat1[0][1] * r2 + mat1[0][2] * r3 - mat1[0][3] * r4
             print("---------------\n")
             print("Определитель = ", r)
@@ -370,9 +420,9 @@ if k == 3:
                 for j in range(n):
                     a.append(int(input()))
                 mat1.append(a)
-            r = np.linalg.det(mat1)
+
             print("---------------\n")
-            print("Результат = ", f"{r:.0f}")
+            print("Результат = ", determinant(mat1))
 
         if m == n == 6:
             print("Введите матрицу -")
@@ -383,7 +433,7 @@ if k == 3:
                 mat1.append(a)
             r = np.linalg.det(mat1)
             print("---------------\n")
-            print("Результат = ", f"{r:.0f}")
+            print("Результат = ", determinant(mat1))
 
         if m == n == 7:
             print("Введите матрицу -")
@@ -394,5 +444,6 @@ if k == 3:
                 mat1.append(a)
             r = np.linalg.det(mat1)
             print("---------------\n")
-            print("Результат = \n", f"{r:.0f}")
+            print("Результат = ", determinant(mat1))
 print("Спасибо за использование нашего калькулятора")
+input()
