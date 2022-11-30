0ec9384e7bed1c2a2134b17a0dbd0fb0e4fc412c
say1dead <107905872+say1dead@users.noreply.github.com>
2022-11-30 20:13:19 +0300

    build.py

--- build.py
+++ build.py
@@ -1,17 +1,59 @@
-unit test:
-
 import unittest
-import calc1
+def ln(x):
+    if x > 0:
+        value = 10 ** 8 * ((x ** (1 / 10 ** 8)) - 1)
+        return value
+    else:
+        return None
+
+
+def log(x, base):
+    if x > 0 and base > 0:
+        value = ln(x) / ln(base)
+        return int(value)
+    else:
+        return None
+
+
+def factorial(x):
+    for i in range(1, x):
+        x *= i
+    return x
+
+
+def sin(x):
+    x *= 3.1415926 / 180
+    value = x
+    sign = -1
+    for i in range(3, 100, 2):
+        value += (x ** i / factorial(i) * sign)
+        sign *= -1
+    return round(value, 4)
+
+
+def cos(x):
+    x *= 3.1415926 / 180
+    value = 1
+    sign = -1
+    for i in range(2, 100, 2):
+        value += (x ** i / factorial(i) * sign)
+        sign *= -1
+    return round(value, 4)
+
+def transpone(a):
+    zip_row = zip(*a)
+    b = [list(row) for row in zip_row]
+    return b
+    
 class Test(unittest.TestCase):
     def test_sin(self):
-      self.assertEqual(calc1.sin(90), 1.0)
+      self.assertEqual(sin(90), 1.0)
     def test_cos(self):
-      self.assertEqual(calc1.cos(180), -1.0)
+      self.assertEqual(cos(180), -1.0)
     def test_log(self):
-      self.assertEqual(calc1.log(32, 2), 5)
+      self.assertEqual(log(32, 2), 5)
     def test_fact(self):
-      self.assertEqual(calc1.factorial(10), 3628800)
+      self.assertEqual(factorial(10), 3628800)
     def test_transpon(self):
-      self.assertEqual(calc1.transpone([[1,2,3],[4,5,6]]), [[1,4],[2,5],[3,6]])
-if name == 'main':
-    unittest.main()
+      self.assertEqual(transpone([[1,2,3],[4,5,6]]), [[1,4],[2,5],[3,6]])
+unittest.main()
